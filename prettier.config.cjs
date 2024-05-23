5aabcc491a2ddcad2c023cc1b3e55daabd021ec1
Miriama Svitkova <miriama.svitkova@reaktor.com>
2024-05-23 12:03:02 +0200

    Initial setup

---
+++ prettier.config.cjs
@@ -0,0 +1,10 @@
+// prettier.config.js
+import tailwind from "prettier-plugin-tailwindcss";
+
+/** @type {import("prettier").Options} */
+export default {
+  // printWidth: 80,
+  // trailingComma: "all",
+  // htmlWhitespaceSensitivity: "css",
+  plugins: [tailwind],
+};
